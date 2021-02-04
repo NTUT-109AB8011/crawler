b177e8d77b297cf1aefe5df89026c5fad6051bf9
terranandes <75928455+terranandes@users.noreply.github.com>
2021-02-05 00:21:29 +0800

    add test.py

---
+++ exercise/test/test.py
@@ -0,0 +1,12 @@
+a='abcde'
+print(a[::-1])
+print(a[-2:0:-1])
+print(a[0:5:1])
+
+crypto_list = ['Yeti', 'Bigfoot', 'Loch Ness Monster']
+print(', '.join(crypto_list))
+poem = 'abcdefbcd'
+print(poem.find('bc'))
+print(poem.rfind('bc'))
+print(poem.rfind('abc'))
+print(poem.count('bc'))
