124e032c1239081dd351d4c1b239a96a7d794234
terranandes <terranandes@gmail.com>
2021-03-02 11:08:59 +0800

    test vim as default editor

--- shared/test.py
+++ shared/test.py
@@ -1,7 +1,12 @@
 #!/opt/homebrew/bin/python3
 
 print("Hello World!")
+<<<<<<< HEAD
 stock = 'tsmc'
 close = 217.5
 print(f'{stock} price: {close}')
 #tsmc price: 217.5'
+=======
+
+#
+>>>>>>> test vim as default editor
