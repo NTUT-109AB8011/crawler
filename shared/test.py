cd328b38a6b353de5e078699f5fc8165ae9ca018
terranandes <75928455+terranandes@users.noreply.github.com>
2021-03-10 02:22:10 +0800

    test variable function argu number

--- shared/test.py
+++ shared/test.py
@@ -1,12 +1,19 @@
 #!/opt/homebrew/bin/python3
 
-print("Hello World!")
-<<<<<<< HEAD
-stock = 'tsmc'
-close = 217.5
-print(f'{stock} price: {close}')
-#tsmc price: 217.5'
-=======
+#print("Hello World!")
+#stock = 'tsmc'
+#close = 217.5
+#print(f'{stock} price: {close}')
+##tsmc price: 217.5'
+def maxx(*value):
+  if len(value) > 0:
+    tmax = value[0]
+  else :
+    tmax = 0
+  for next in value[1:] :
+    if next > tmax:
+      tmax = next
+  return tmax
 
-#
->>>>>>> test vim as default editor
+#print(maxx(1,2,3,4,5))
+print(maxx())
