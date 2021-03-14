f8c3f8dbb9e0a698bb69e353e36fec53f6fba343
terranandes <75928455+terranandes@users.noreply.github.com>
2021-03-14 19:45:02 +0800

    update

--- exercise/test/main.py
+++ exercise/test/main.py
@@ -2,6 +2,30 @@
 #import mymodule
 
 #mymodule.greeting("Jonathan")
-import platform
-x=platform.system()
-print(x)
+#import platform
+#x=platform.system()
+#print(x)
+#import datetime
+
+#x = datetime.datetime.now()
+#print(x)
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
--- exercise/test/test.py
+++ exercise/test/test.py
@@ -5,15 +5,22 @@
 #close = 217.5
 #print(f'{stock} price: {close}')
 ##tsmc price: 217.5'
-def maxx(*value):
-  if len(value) > 0:
-    tmax = value[0]
-  else :
-    tmax = 0
-  for next in value[1:] :
-    if next > tmax:
-      tmax = next
-  return tmax
-
+#def maxx(*value):
+#  if len(value) > 0:
+#    tmax = value[0]
+#  else :
+#    tmax = 0
+#  for next in value[1:] :
+#    if next > tmax:
+#      tmax = next
+#  return tmax
+#
 #print(maxx(1,2,3,4,5))
-print(maxx())
+#print(maxx())
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
