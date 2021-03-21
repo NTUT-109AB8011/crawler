d1296e88c134ae6a5532056b843840bd18dd44e5
terranandes <terranandes@gmail.com>
2021-03-21 16:24:41 +0800

    add main shared python package versitile finance crawler add test crawler python file

--- exercise/test/test_crawler.py
+++ exercise/test/test_crawler.py
@@ -1,4 +1,17 @@
 import urllib.request as request
-with request.urlopen(xxx) as response
-  data = response.read()
-print(data)
+import json
+src="https://data.taipei/api/v1/dataset/296acfa2-5d93-4706-ad58-e83cc951863c?scope=resourceAquire"
+with request.urlopen(src) as response:
+  #data = response
+  #data = response.read()
+  #data = response.read().decode("utf-8")
+  data = json.load(response)
+  #data = json.loads(response.read().decode("utf-8"))
+
+clist = data["result"]["results"]
+with open("data.txt", "w", encoding="utf-8") as file:
+  for company in clist:
+    file.write(company["公司地址"]+"\n")
+  #print (addr_dict)
+  #print (addr_dict["公司名稱"])
+  #print (addr_dict["公司地址"])
